c7c95eaa77c01613c50dc5cd341e44addfa2cb93
Clement Liscoet <cliscoet@apple.com>
2022-09-01 16:43:57 +0100

    done

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from py_utilities import __version__
+
+setup(
+    name='py_utilities',
+    version=__version__,
+
+    url='https://github.com/clemoni/py_utilities',
+    author='Clement Liscoet',
+    author_email='clement.liscoet@gmail.com',
+
+    py_modules=['py_utilities'],
+)
